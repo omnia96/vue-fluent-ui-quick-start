83993cffead572dcea14f57927c0e22108676f66
city-hunter <dongjun1997@outlook.com>
2022-03-18 18:34:25 +0800

    feat: 初始化项目

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import {fluentButton, fluentCard, fluentTextField, provideFluentDesignSystem} from "@fluentui/web-components";
 
+provideFluentDesignSystem().register(
+    fluentButton(),
+    fluentCard(),
+    fluentTextField(),
+);
 createApp(App).use(store).use(router).mount('#app')
